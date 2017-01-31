417f417cdd7b9b250c911dd24d6ea3cebdea45fe
egpavelka <pav@egpavelka.com>
2017-01-30 23:56:30 -0500

    text fixes

--- app/src/assets/content_image-page.js
+++ app/src/assets/content_image-page.js
@@ -5,7 +5,7 @@ export const ImgContent = [
     class: 'image-page-1',
     img: 'lrd1.jpg',
     header: 'Les Rallizes Dénudés',
-    description: <span>Described by many as the ultimate cult band, Les Rallizes D&eacute;nud&eacute;s undeniably earned a place among the very loudest, most experimental pioneers of the Japanese underground psychedelic and noise rock scene. Despite a career spanning four decades, the band remains shrouded in mystery, having shunned the press, the public, and the music recording industry for the vast majority of that time.</span>
+    description: <span>Described by many as the ultimate cult band, Les Rallizes D&eacute;nud&eacute;s undeniably earned a place among the very loudest, most experimental pioneers in the history of the Japanese underground. Despite a career spanning four decades, the band remains shrouded in mystery, having shunned the press, the public, and the music recording industry for the vast majority of that time.</span>
   },
   {
    class: 'image-page-2',
@@ -16,7 +16,7 @@ export const ImgContent = [
  {
   class: 'image-page-1',
   img: 'lrd3.jpg',
-  header: 'radical politics',
+  header: 'political controversy',
   description: <span>in 1970, the original bass player, moriaki wakabayashi, joined the red army faction in the hijacking of japan airlines flight 351. the plane was diverted to pyongyang, north korea, and the hijackers were granted asylum by the country's communist regime.</span>
 },
 {
